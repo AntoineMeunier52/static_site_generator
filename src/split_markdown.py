d0260e00923444bf676aee7866764c8d779eecc7
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-04-27 12:08:25 +0200

    add pattern regex for images and links

--- src/split_markdown.py
+++ src/split_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_node = []
@@ -19,3 +20,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_node.append(TextNode(split_text[i], text_type))
     return new_node
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
