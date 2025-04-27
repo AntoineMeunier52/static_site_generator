9aa2c747714d805e58d95082d4d69c689c0339ba
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-04-27 20:55:37 +0200

    add directory for github static site render

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import TextType
 from textnode import TextNode
 from markdown_to_html import markdown_to_html
+from gencontent import generate_pages_recursive
 import os
 import shutil
+import sys
 
 def create_public_dir(static_path, public_path):
     curr_path = os.listdir(static_path)
@@ -24,45 +26,21 @@ def del_public_dir():
         print("delete public")
         shutil.rmtree("public")
 
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line[2:]
-
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    f_markdown = open(from_path)
-    f_template = open(template_path)
-
-    content_markdown = f_markdown.read()
-    content_template = f_template.read()
-
-    f_markdown.close()
-    f_template.close()
-
-    html_node = markdown_to_html(content_markdown)
-    html_content = html_node.to_html()
-
-    page_title = extract_title(content_markdown)
-
-    content_template_with_title = content_template.replace("{{ Title }}", page_title)
-    new_html = content_template_with_title.replace("{{ Content }}", html_content)
-
-    dest_dir_path = os.path.dirname(dest_path)
-    if dest_dir_path != "":
-        os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(new_html)
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     del_public_dir()
     create_public_dir("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 if __name__ == "__main__":
     main()
